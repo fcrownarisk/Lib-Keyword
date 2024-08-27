5984d1f06ec98dcfa00350ec08083d7975abb524
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-27 17:18:37 +0800

    Add files via upload

---
+++ lib and keyword.c
@@ -0,0 +1,47 @@
+
+#include "stdio.h" //Standard Input and Output 
+//Formatted Output-Printf
+//Formatted Input-Scanf
+//Variable-length Argument Lists
+//Error Handling Stderr and Exit
+//Line Input and Output
+// 7.8.2 Cahracter Class Testing and Conversation
+//isalpha isupper is lower isdigit isalnum 
+//non-zero if c is a letter, digit, or underscore
+// 7.8.3 Ungetc 7.8.4Command Execution 7.8.5 Storage Management
+// 7.8.6 Mathmatical Functions sin cos atan2 exp log sqrt fabs
+#include "stdlib.h" //File Access
+#include "string.h" //
+#include "stddef.h" //
+#include "stdarg.h" //
+
+#include "assert.h" //
+#include "ctype.h" //
+#include "errno.h" //
+#include "float.h" //
+
+#include "limits.h" //
+#include "locale.h" //
+#include "setjmp.h" //
+#include "signal.h" //
+
+//Chapter8 The UNIX System Interface
+//File Desscriptors 8.2 Low Level I/O --Read and Write
+//Open Creat Close Unlink
+// 8.4Random Access-Lseek   8.5 Example --An Implementation of Fopen and Gect   8.6 Example--Listing Directories
+//
+
+#include "time.h"
+
+//Headers may be included in any order and any number of times.A header must be
+//included outside of any external declarations or definition and before any use of anything it declares
+//A header not need be a source file
+
+//Keyword in C language
+//auto double int struct break else long switch case enum register typedef
+//char extern return union const float short unsigned continue for signed 
+
+
+//C99 Keyword
+//void default goto sizeof volatile do if static while
+//int float double char short unsigned long signed void
