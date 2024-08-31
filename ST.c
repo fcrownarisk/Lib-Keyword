0a2fa364de694526ad77309750163cb944941fe5
fcrownarisk <155938693+fcrownarisk@users.noreply.github.com>
2024-08-31 09:32:17 +0800

    Create ST.c

---
+++ ST.c
@@ -0,0 +1,24 @@
+#include "stdarg.h"
+#include "stdlib.h"
+#include "stddef.h"
+#include "string.h"
+#include "sttdio.h"
+#define A Area
+#define T Temperature
+#define c 299792458
+#define G 0.00000 00000 066743
+#define k 0.00000 00000 00000 00000 001 380649
+#define H 0.00000 00000 00000 00000 00000 0000 0001 054571817
+void ST(){
+  int S = [(A*k*c*c*c)/(4*H*G)]
+  int T = [(h*c*c)/(8*H*k*G*M)]
+}
+void S(){
+  typedef s.targ
+  typedef s.tlib
+  typedef s.tddef
+}
+void T(){
+  typeof st.ring
+  typeof st.dio
+}
